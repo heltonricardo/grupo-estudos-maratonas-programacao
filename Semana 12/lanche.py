c39173fecac4fd8057f073c3fc0a4784a4ee88dc
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-18 10:27:33 -0300

    Semana 12

---
+++ Semana 12/lanche.py
@@ -0,0 +1,8 @@
+c, q = [int(x) for x in input().split()]
+if c == 1: v = q * 4
+elif c == 2: v = q * 4.5
+elif c == 3: v = q * 5
+elif c == 4: v = q * 2
+else: v = 1.5
+s = f'Total: R $ {v:.2f}'.replace('.', ',')
+print(s)
